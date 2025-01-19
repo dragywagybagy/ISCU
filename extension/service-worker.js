c31d26e71e29279c7434070a2142d29875dbba21
joelmathewsssss <joelalexmat@gmail.com>
2025-01-19 10:22:01 +0000

    commit

--- extension/service-worker.js
+++ extension/service-worker.js
@@ -2,7 +2,7 @@ chrome.action.onClicked.addListener(tab => {
     chrome.scripting.executeScript({
         target : {tabId : tab.id},
         func: () => {
-            alert('You"ve completed your assignment!')
+            alert('Youve completed your assignment!')
         }
     })
 })
